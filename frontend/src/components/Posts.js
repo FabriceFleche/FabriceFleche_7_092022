a138c963e38ccae0e0afbce62da8f51733b7ed2d
Fabrice Fleche <fabrice.fleche@free.fr>
2022-10-16 22:10:53 +0200

    Creation debut Fetch

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
-const Posts = () => {
-    useEffect(() => {
-        axios.get("")
-    }, [])
+//const baseURL = "http://192.168.0.187:3000/api/posts/"
+const baseURL1 = "http://localhost:3000/api/posts/"
+//const baseURL2 = "https://restcountries.com/v3.1/all"
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQsImlhdCI6MTY2NTkxNDUwMywiZXhwIjoxNjY2MDAwOTAzfQ.mii5ysMCGKgIORXfgNNxwJaLwItRIfa1LyaUMgvAGe0"
+const config = { headers: { Authorization: `javascript">Bearer ${token}` } };
+
+const Posts = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios({
+            url: baseURL1,
+            method: "GET",
+            Authorization: config
+
+        })
+            .then((res) => { setData(res.data) })
+        //.catch((err) => { err })
+    }, []);
 
     return (
-        <div className='posts'>
-
+        <div>
+            <ul>
+                {
+                    //data.map((posts) => (<li>{posts.translations.fra.common}</li>))
+                    data.map((posts) => (<li>{posts}</li>))
+                }
+            </ul>
         </div>
     );
 };
--- frontend/src/components/SeConnecter.js
+++ frontend/src/components/SeConnecter.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+//import axios from "axios";
+function UserLogin() {
+    const baseURL = "http://localhost:3000/api/auth/login"
+    const [data, setData] = useState([]);
 
-const SeConnecter = () => {
+    useEffect(() => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title: "" })
+        };
+        fetch(baseURL, requestOptions)
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+
+
     return (
-        <div>
+        < div >
             <div className="seConnecter">
                 <ul>
                     <NavLink to="/home">
                         <li>Se connecter</li>
                     </NavLink>
                 </ul>
+                <div>User Id : {data}</div>
             </div>
-        </div>
-    );
-};
+        </div >
 
-export default SeConnecter;
+    );
+}
+
+export default UserLogin;
