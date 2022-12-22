47c466ee42c2f4e4ea7a0213e1e4a186ac1f2409
Fabrice Fleche <fabrice.fleche@free.fr>
2022-12-22 20:18:42 +0100

    Rajout commentaires

--- backend/routes/like.js
+++ backend/routes/like.js
@@ -3,9 +3,16 @@ const routerlike = express.Router();
 const likeCtrl = require('../controllers/like');
 const auth = require('../middleware/auth');
 
+// Creation de la route pour la recuperation du nombre des likes par posts avec vérification du token
 routerlike.post('/', auth, likeCtrl.like);
+
+// Creation de la route pour le stockage dans la table userslikes du like d'un user avec vérification du token
 routerlike.post('/likeUser', auth, likeCtrl.likeUser);
+
+// Creation de la route pour la recuperation de tous les posts aimés par un user avec vérification du token
 routerlike.get('/', auth, likeCtrl.getLikes);
+
+// Creation de la route pour la suppression du like par l'user dans la table userslikes avec vérification du token
 routerlike.delete('/', auth, likeCtrl.deleteLike);
 
 module.exports = routerlike;
