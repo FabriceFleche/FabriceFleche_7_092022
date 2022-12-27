4ca8a17abc258ba9412eb09a39b95bcec9143fc3
Fabrice Fleche <fabrice.fleche@free.fr>
2022-12-27 11:14:13 +0100

    Maj Gestion email identique

--- frontend/src/components/createUser/UserCreate.js
+++ frontend/src/components/createUser/UserCreate.js
@@ -7,9 +7,13 @@ function userCreate(enteredName, enteredEmail, enteredPassword) {
     };
     fetch(baseURL, requestOptions)
         .then(response => response.json())
-        .then(() => {
-            alert('Votre compte à été créé, vous pouvez maintenant vous connecter avec votre email et votre mot de passe')
-        });
+        .then((data) => {
+            if (data.message === undefined) {
+                alert('Un compte existe deja avec cet email')
+            } else { alert('Votre compte à été créé, vous pouvez maintenant vous connecter avec votre email et votre mot de passe') }
+        })
+        .catch(function (err) { return err.status(500).json({ err }) });
 }
+
 
 export default userCreate;
